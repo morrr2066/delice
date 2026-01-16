12a925e1a4fb98e2a5bd066c2d23682667ea833e
Marwan Essam <marwanessam368@gmail.com>
2026-01-16 21:07:23 +0200

    user try

--- delice/urls.py
+++ delice/urls.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from django.urls import path,include
 from accounts import views
+from django.contrib.auth import get_user_model
+
+
+def create_admin_once():
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('owner', 'admin@example.com', '2691999')
+        print("Done! Admin created.")
+
+# بنادي الدالة دي عشان تشتغل أول ما السيرفر يقوم
+try:
+    create_admin_once()
+except:
+    pass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
